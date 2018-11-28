a79a5caa22d94c0915438e3de86622744ee19340
Etienne Châtaignier <etienne.chataignier@biig.fr>
2018-11-28 16:58:30 +0100

    Set package.json.dist

--- bin/build.sh
+++ bin/build.sh
@@ -5,5 +5,5 @@ cp -R src/models/files dist/models/ && \
 cp -R src/openapi/files dist/openapi/ && \
 cp -R src/openapi/files dist/openapi/ && \
 cp src/collection.json dist/ && \
-cp package.json dist/ && \
+cp package.json.dist dist/package.json && \
 cp README.md dist/
